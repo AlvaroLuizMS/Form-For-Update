f150549fd9e5e796eab3aa1e27bc18898840ccfd
Alvaro Luiz <56884028+AlvaroLuizMS@users.noreply.github.com>
2021-03-06 18:18:31 -0300

    'Update inputs'

--- js/form.js
+++ js/form.js
@@ -1,19 +1,26 @@
 
   
-  //* função de validação dos dados do formulario*//
+  // função de validação dos dados do formulario
     function Validate_Envio() {
-        var formulario = document.forms["Update"] //recebe o formulario 
-        var nome = formulario.nome.value  //cria a variavel e atribui a ela o valor digitado 
+        var formulario = document.forms["Update"]   //recebe o formulario 
+        var nome = formulario.nome.value            //cria a variavel e atribui a ela o valor digitado 
         var cpf = formulario.cpf.value
         var telefone = formulario.tel.value
         var cep = formulario.cep.value
         
+        /* 
+            -indexOf verifica se há espaço entre os caracteres digitados
 
-        if (nome.indexOf(" ") == -1){           //.indexOf verifica se há espaço entre os caracteres digitados
+            -retorna falso para que o formulario nao seja enviado
+
+            -length verifica o comprimento dos caracteres digitados 
+        */
+        
+        if (nome.indexOf(" ") == -1){           
             alert("Preencha o nome completo")
-            return false                        //retorna falso para que o formulario nao seja enviado 
+            return false                        
         }
-        if (cpf.length != 11){                  //.length verifica o comprimento dos caracteres digitados
+        if (cpf.length != 11){                  
             alert("Preencha um CPF valido!")
             return false
         }
